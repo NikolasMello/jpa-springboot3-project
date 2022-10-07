da872fc97cc23d384d55681047aea802d0ef227a
NikolasMello <nikolasmarcos@gmail.com>
2022-10-07 10:04:43 -0300

    Order, Instant, ISO 8601 pt1

--- src/main/java/com/nikolasmello/projetosb/entities/User.java
+++ src/main/java/com/nikolasmello/projetosb/entities/User.java
@@ -5,11 +5,15 @@ package com.nikolasmello.projetosb.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id //deifinido como chave primary key
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,6 +22,10 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
 
     public User() {
 
@@ -69,6 +77,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
