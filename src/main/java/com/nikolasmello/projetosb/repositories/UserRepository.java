adeb919e15f028e103b6b687101e5ccd62fff9a2
NikolasMello <nikolasmarcos@gmail.com>
2022-10-07 09:43:54 -0300

    Service layer, component registration

--- src/main/java/com/nikolasmello/projetosb/repositories/UserRepository.java
+++ src/main/java/com/nikolasmello/projetosb/repositories/UserRepository.java
@@ -3,6 +3,8 @@ package com.nikolasmello.projetosb.repositories;
 import com.nikolasmello.projetosb.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//Quando a interface já está herdando um componente registrado do spring como o caso abaixo
+//não é necessário usar a annotation @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
