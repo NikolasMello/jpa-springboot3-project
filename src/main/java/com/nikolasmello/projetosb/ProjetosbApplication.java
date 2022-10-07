815f1fef9ad8192368f4eb6988eb852e6b59fc98
NikolasMello <nikolasmarcos@gmail.com>
2022-10-07 01:31:06 -0300

    user entity and resource

--- src/main/java/com/nikolasmello/projetosb/ProjetosbApplication.java
+++ src/main/java/com/nikolasmello/projetosb/ProjetosbApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjetosbApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProjetosbApplication.class, args);
 	}
 
