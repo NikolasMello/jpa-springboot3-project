8c370d0b3dba73a1bf7454846b711949c170586f
NikolasMello <nikolasmarcos@gmail.com>
2022-10-07 01:12:08 -0300

    first commit

---
+++ src/main/java/com/nikolasmello/projetosb/ProjetosbApplication.java
@@ -0,0 +1,13 @@
+package com.nikolasmello.projetosb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetosbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetosbApplication.class, args);
+	}
+
+}
